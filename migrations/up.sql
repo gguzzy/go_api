35c29937021eb0c3b49101d1fd95600ce4f60ec1
mapinto25 <mapinto@ucsd.edu>
2021-01-01 02:18:07 -0800

    added sql

---
+++ migrations/up.sql
@@ -0,0 +1,6 @@
+create table employee (
+	id int primary key,
+    employee_name varchar (60),
+    employee_age int,
+    employee_salary float
+);
--- server.go
+++ server.go
@@ -1,9 +1,12 @@
 package main
-
+ 
 import (
-  "net/http"
-  "github.com/labstack/echo"
-  "github.com/labstack/echo/middleware"
+            "database/sql"
+            "fmt"
+            _ "github.com/go-sql-driver/mysql"
+            "github.com/labstack/echo"
+            "github.com/labstack/echo/middleware"
+            "net/http"
 )
 
 func main() {
@@ -14,8 +17,56 @@ func main() {
   e.Use(middleware.Logger())
   e.Use(middleware.Recover())
 
+  e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+    AllowOrigins: []string{"*"},
+    AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+}))
+
+type Employee struct {
+  Id     string `json:"id"`
+  Name   string `json:"employee_name"`
+  Salary string `json: "employee_salary"`
+  Age    string `json : "employee_age"`
+}
+type Employees struct {
+  Employees []Employee `json:"employee"`
+}
+
+
+db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/dummy_db")
+	if err != nil {
+				fmt.Println(err.Error())
+	} else {
+				fmt.Println("db is connected")
+	}
+	defer db.Close()
+	// make sure connection is available
+	err = db.Ping()
+	if err != nil {
+				fmt.Println(err.Error())
+	}
+
+
   // Routes
   e.GET("/", hello)
+
+e.GET("/employee/:id", func(c echo.Context) error {
+	requested_id := c.Param("id")
+	fmt.Println(requested_id)
+	var name string
+	var id string
+	var salary string
+	var age string
+ 
+	err = db.QueryRow("SELECT id,employee_name, employee_age, employee_salary FROM employee WHERE id = ?", requested_id).Scan(&id, &name, &salary, &age)
+ 
+	if err != nil {
+				fmt.Println(err)
+	}
+ 
+	response := Employee{Id: id, Name: name, Salary: salary, Age: age}
+	return c.JSON(http.StatusOK, response)
+})
 
   // Start server
   e.Logger.Fatal(e.Start(":1323"))
